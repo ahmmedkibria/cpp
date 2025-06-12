0ca54d7d174ea53dc442f7af8ccd5e624e44a1b4
ahmmedkibria <153896996+ahmmedkibria@users.noreply.github.com>
2025-06-12 00:19:29 -0500

    Update sort_students_by_marks_and_roll.cpp

--- sort_students_by_marks_and_roll.cpp
+++ sort_students_by_marks_and_roll.cpp
@@ -1,18 +1,17 @@
+//   prac prob : Make a class named Student.
+//   Write a program to take a positive integer N as input and make an Student array of size N.
+//   Student{ name; roll; marks; }
 
-// prac prob : Make a class named Student.
-//Write a program to take a positive integer N as input and make an Student array of size N.
-// Student{ name; roll; marks; }
-
-// Your task is to sort the Students data according to the marks in descending order.
-//If multiple students have the same marks then sort them according to the roll in ascending
-//order as the roll will be unique.
-//Note: name will not contain any spaces.
-
+//  Your task is to sort the Students data according to the marks in descending order.
+//  If multiple students have the same marks then sort them according to the roll in ascending
+//  order as the roll will be unique.
+//  Note: name will not contain any spaces.
 
 
 #include<iostream>
 #include <algorithm> // Required for sort()
 using namespace std;
+
 
 
 class Student {
@@ -47,6 +46,23 @@ int main(){
     for (int i=0; i<n; i++){
         cout << arr[i].name << " "<< arr[i].roll << " " <<arr[i].marks <<endl;
     }
-    
     return 0;
 }
+
+
+//input
+
+//5
+//Asif 29 95
+//Sakib 55 86
+//Zubair 57 86
+//Ahsan 39 86
+//Joy 12 99
+
+// output
+
+//Joy 12 99
+//Asif 29 95
+//Ahsan 39 86
+//Sakib 55 86
+//Zubair 57 86

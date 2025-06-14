46a9f1d34192fc17b2b5f42c939a715072dde203
ahmmedkibria <153896996+ahmmedkibria@users.noreply.github.com>
2025-06-14 12:48:36 -0500

    Create string2.cpp

---
+++ bascis_c++/string2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "give input : Hello World" <<endl;
+    string s;
+    cin >> s;
+    
+    cout <<  "output :" <<  s  << endl;
+    cout << " to get strings after space use getline( cin , s); "<< endl;
+    
+    
+    return 0;
+}

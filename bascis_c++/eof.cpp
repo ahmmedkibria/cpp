60d54ea24364752b4802ca5538f19f5af2c24bbf
ahmmedkibria <153896996+ahmmedkibria@users.noreply.github.com>
2025-06-14 09:12:13 -0500

    Create eof.cpp

---
+++ bascis_c++/eof.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    
+// continue accepting inputs till user stops
+    
+    int x;
+    while ( cin >> x){
+        
+        cout << x << endl;
+    }
+    
+    return 0;
+}

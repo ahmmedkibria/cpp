16e0106f90fd531b47bbd64964e84821548d4fa3
ahmmedkibria <153896996+ahmmedkibria@users.noreply.github.com>
2025-06-14 09:31:54 -0500

    Create ternary_operator.cpp

---
+++ bascis_c++/ternary_operator.cpp
@@ -0,0 +1,12 @@
+// ternery operator is kind of shortcut of if_else
+// (condition) ? T: F
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 19;
+    x%2 == 0 ?   cout << "Even\n" :  cout << "odd\n";
+    
+    
+    return 0;
+}
